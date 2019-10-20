d0ee0e863af148c7378961a7035292986907e095
Wiston <wistonmiguel@gmail.com>
2019-10-20 15:05:44 -0600

    finanlizado crud almacen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Bienvenido {{ Auth::user()->name }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,9 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    Bienvenido a su Panel de Administración Nueva Inglaterra!
+                    <div class="my-4" style="text-align: center;">
+                    <img class="img-fluid" src="{{ asset('./img/logo.png') }}" />
+                    </div>
                 </div>
             </div>
         </div>
